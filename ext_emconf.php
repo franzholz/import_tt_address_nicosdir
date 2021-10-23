94ff7093668510e47a9fce2b6717751ce93b99ae
Franz Holzinger <franz.holzinger@festwein.de>
2021-10-23 15:05:26 +0200

    restructuring

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'nicosdirectory into tt_address and sys_category',
 	'description' => 'This extends the import extension for the conversion from nicosdirectory to tt_address',
 	'category' => 'backend',
@@ -12,16 +12,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'author_company' => '',
 	'version' => '0.1.2',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => [
+		'depends' => [
 			'php' => '5.5.0-7.99.99',
 			'typo3' => '7.6.0-9.99.99',
 			'import' => '0.4.0-0.0.0',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+		],
+		'conflicts' => [
+		],
+		'suggests' => [
+		],
+	],
+];
 
